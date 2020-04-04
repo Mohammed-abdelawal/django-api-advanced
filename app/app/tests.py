654daaf47e33926a97bbfc1e4b51fa303cfeb398
Mohammed-abdelawal <mohammedabdelawaldeveloper@gmail.com>
2020-04-05 00:15:19 +0200

    test

--- app/app/tests.py
+++ app/app/tests.py
@@ -8,10 +8,10 @@ def add(x, y):
 
 class CalcTests(TestCase):
 
-    def test_add_numbers(self):
+    def test_add_numbers_ok(self):
         """Test that values are added together"""
         self.assertEqual(add(3, 8), 11)
 
-    def test_add_numbers_(self):
+    def add_numbers_notok(self):
         """Test that values are added together"""
         self.assertEqual(add(10, 8), 11)
