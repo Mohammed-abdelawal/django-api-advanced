5a1e4fc9d46c4b89d40f3268239cfd1c7331cc1d
Mohammed-abdelawal <mohammedabdelawaldeveloper@gmail.com>
2020-04-14 02:24:46 +0200

    database docker and setup app to wait for the database to be available

--- Dockerfile
+++ Dockerfile
@@ -4,10 +4,13 @@ ENV PYTHONUNBUFFERED 1
 
 # Install dependencies
 COPY ./requirements.txt /requirements.txt
+RUN apk update
 RUN apk add --update --no-cache postgresql-client
 RUN apk add --update --no-cache --virtual .tmp-build-deps \
       gcc libc-dev linux-headers postgresql-dev
-RUN pip install -r /requirements.txt
+
+RUN pip install -r /requirements.txt	
+
 RUN apk del .tmp-build-deps
 
 # Setup directory structure
--- app/app/settings.py
+++ app/app/settings.py
@@ -79,11 +79,13 @@ AUTH_USER_MODEL = 'core.User'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': os.environ.get('DB_HOST'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASS'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
